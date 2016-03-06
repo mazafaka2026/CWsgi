b09f9fd87c93b4b83ebe6ea145039d6e79739dfe
Fpk <dmitiry@nkdn.ru>
2016-03-06 17:10:32 +0300

    More

--- web/hello.py
+++ web/hello.py
@@ -1,3 +1,4 @@
-def app(env, start_response):
-    start_response('200 OK', [('Content-Type', 'text/plain')])
-    return  ['Hello, world!']
+def app(env, start_response):                                                   
+    start_response('200 OK', [('Content-Type', 'text/plain')])                  
+    return  [x + "\n\r" for x in env["QUERY_STRING"].split("&")]                
+
