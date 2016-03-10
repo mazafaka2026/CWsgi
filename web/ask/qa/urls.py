ebf5bf533bdff6f29535939cf56ed124bea9628d
root <root@flopik>
2016-03-10 16:39:53 +0300

    More urls

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -5,5 +5,11 @@ urlpatterns = patterns('qa',
     # Examples:
     # url(r'^$', 'ask.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
-    url(r'question/(\d+)/', 'views.test'),
+    url(r'^$', 'views.test', name='index'),
+    url(r'^login/', 'views.test', name='login'),
+    url(r'^signup/', 'views.test', name='signup'),
+    url(r'^question/(?P<id>[0-9]+)/$', 'views.test', name='question'),
+    url(r'^ask/', 'views.test', name='ask'),
+    url(r'^popular/', 'views.test', name='popular'),
+    url(r'^new/', 'views.test', name='new'), 
 )
